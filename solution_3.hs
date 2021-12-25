7faace8d63b0f0b0257bdaec8d06e50c1dfe8744
Rory Sametz <rorysametz@gmail.com>
2021-12-24 16:36:35 -0800

    Impleented Solution 3

---
+++ solution_3.hs
@@ -0,0 +1,36 @@
+import qualified Data.List as List
+
+countBinary :: (Int, Int) -> Char -> (Int, Int)
+countBinary (a,b) '0' = (a + 1, b)
+countBinary (a,b) '1' = (a, b + 1)
+countBinary _ x = error ("Non-binary character found: " ++ show x)
+
+binarySequence :: [Int]
+binarySequence = map (2^) [0..]
+
+boolToBin :: Bool -> Char
+boolToBin True = '1'
+boolToBin False = '0'
+
+binToNum :: Char -> Int
+binToNum '0' = 0
+binToNum '1' = 1
+binToNum _ = error "Not a binary number"
+
+binToInt :: String -> Int
+binToInt str = let binaryNum = map binToNum (reverse str)
+               in sum (map (uncurry (*)) (zip binarySequence binaryNum))
+
+main = do
+  let tinput = List.transpose input
+  let count = map (foldl countBinary (0,0)) tinput
+  let rate f = map boolToBin (map (uncurry f) count)
+  let gammaRate = rate (>)
+  let epsilonRate = rate (<)
+  let powerConsumption = (binToInt gammaRate) * (binToInt epsilonRate)
+  print ("Gamma Rate: " ++ gammaRate)
+  print ("Epsilon Rate: " ++ epsilonRate)
+  print ("Power Consumption: " ++ show powerConsumption)
+  where
+    input = ["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
+
