4c251aab943897b76b1e5f05caa0759fbdfbfcce
Sandy Maguire <sandy@sandymaguire.me>
2021-12-30 12:35:59 -0800

    Deal with parser failure

--- solution_2.hs
+++ solution_2.hs
@@ -1,5 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 
+import Data.Maybe (mapMaybe)
+
 getPrefix :: String -> String
 getPrefix = takeWhile (/= ' ')
 
@@ -8,18 +10,23 @@ getValue command prefix = read (drop (1 + length prefix) command)
 
 data Direction = Up | Down | Forward
 
-parseDirection :: String -> Direction
-parseDirection "up" = Up
-parseDirection "down" = Down
-parseDirection "forward" = Forward
+parseDirection :: String -> Maybe Direction
+parseDirection "up" = Just Up
+parseDirection "down" = Just Down
+parseDirection "forward" = Just Forward
+parseDirection _ = Nothing
 
 data MoveCommand = MoveCommand
   { moveDirection :: Direction
   , moveCount :: Int
   }
 
-parseMoveCommand :: String -> MoveCommand
-parseMoveCommand command = MoveCommand (parseDirection prefix) value
+parseMoveCommand :: String -> Maybe MoveCommand
+parseMoveCommand command = do
+  -- If we fail to parse the direction, we will fail to parse the entire move
+  -- command
+  direction <- parseDirection prefix
+  pure $ MoveCommand direction value
   where
     prefix = getPrefix command
     value = getValue command prefix
@@ -31,7 +38,9 @@ move (x, y) (MoveCommand Forward value) = (x + value, y)
 
 main :: IO ()
 main = do
-  let output = foldl move initPos $ fmap parseMoveCommand input
+  let output = foldl move initPos
+               -- mapMaybe will throw away any Nothings it comes across
+             $ mapMaybe parseMoveCommand input
   print output
   where initPos = (0,0)
         input   = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
