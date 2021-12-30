60481c87a8909b7a82294abeed395a7582ae84e7
Sandy Maguire <sandy@sandymaguire.me>
2021-12-30 12:15:41 -0800

    Prefer to bind variables in let instead of where

--- solution_1.hs
+++ solution_1.hs
@@ -6,5 +6,6 @@ collect xs = sum $ zipWith (\x y -> if (x < y) then 1 else 0) xs $ drop 1 xs
 
 main :: IO ()
 main = do
-  let output = collect input where input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+  let input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+      output = collect input
   print output
