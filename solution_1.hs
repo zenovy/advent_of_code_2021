88f21ae28907120d520d4c1f96219eef0033acce
Sandy Maguire <sandy@sandymaguire.me>
2021-12-30 12:12:58 -0800

    Enable -Wall, and then clean up the warnings

--- solution_1.hs
+++ solution_1.hs
@@ -1,8 +1,11 @@
+{-# OPTIONS_GHC -Wall #-}
+
 collect :: [Int] -> Int
 collect []       = 0
-collect (x:[])   = 0
+collect (_:[])   = 0
 collect (x:y:xs) = (if (x < y) then 1 else 0) + collect (y:xs)
 
+main :: IO ()
 main = do
   let output = collect input where input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
   print output
