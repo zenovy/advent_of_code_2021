b466ec429319130b172a357aa1156f380f7c2a40
Sandy Maguire <sandy@sandymaguire.me>
2021-12-30 12:27:08 -0800

    Separate parsing from moving

--- solution_2.hs
+++ solution_2.hs
@@ -6,17 +6,26 @@ getPrefix (x:xs)   = x: getPrefix xs
 getValue :: String -> String -> Int
 getValue command prefix = read (drop (1 + length prefix) command)
 
-move :: (Int, Int) -> String -> (Int, Int)
-move (x, y) command
+data MoveCommand = MoveCommand
+  { moveDirection :: String
+  , moveCount :: Int
+  }
+
+parseMoveCommand :: String -> MoveCommand
+parseMoveCommand command = MoveCommand prefix value
+  where
+    prefix = getPrefix command
+    value = getValue command prefix
+
+move :: (Int, Int) -> MoveCommand -> (Int, Int)
+move (x, y) (MoveCommand prefix value)
   | prefix == "up"      = (x, y - value)
   | prefix == "down"    = (x, y + value)
   | prefix == "forward" = (x + value, y)
-  | otherwise           = error ("Command not defined in string '" ++ command ++ "'")
-  where prefix = getPrefix command
-        value = getValue command prefix
+  | otherwise           = error ("Command not defined in string '" ++ prefix ++ "'")
 
 main = do
-  let output = foldl move initPos input
+  let output = foldl move initPos $ fmap parseMoveCommand input
   print output
   where initPos = (0,0)
         input   = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
